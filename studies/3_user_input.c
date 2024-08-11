8388201ab4d785c0e93e886a418163befc09a06d
Karoline Cruz <karollcruz@hotmail.com>
2024-08-11 16:30:19 +0100

    feat: scanf functionality for read user input.

---
+++ studies/3_user_input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  int age;
+  float salary;
+  char gender;
+
+  printf("How old are you?");
+  scanf("%d", &age);
+
+  printf("What's you salary?");
+  scanf("%f", &salary);
+
+  printf("What's your gender F|M|O?");
+  scanf(" %c", &gender);
+
+  printf("Is It Time For a Career Change?");
+
+  printf("You are %d years old, identifies with the %c gender and receives a salary of %.2f euros", age, gender, salary);
+
+  return 0;
+}

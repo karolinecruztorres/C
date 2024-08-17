5f555cd2137b25d00316e4af57a50f6384ff3c46
Karoline Cruz <karollcruz@hotmail.com>
2024-08-17 20:54:47 +0100

    chore: spell check.

--- studies/6_math_library.c
+++ studies/6_math_library.c
@@ -12,12 +12,12 @@ int main(void)
   scanf("%f", &angle_deg);
 
   angle_rad = PI_RAD * angle_deg / PI_DEGREES;
-  printf("The sine of %.1f e %.4f. \n", angle_deg, sin(angle_rad));
-  printf("The cosine of %.1f e %.4f. \n", angle_deg, cos(angle_rad));
-  printf("The tangent of %.1f e %.4f. \n", angle_deg, tan(angle_rad));
+  printf("The sine of %.1f is %.4f. \n", angle_deg, sin(angle_rad));
+  printf("The cosine of %.1f is %.4f. \n", angle_deg, cos(angle_rad));
+  printf("The tangent of %.1f is %.4f. \n", angle_deg, tan(angle_rad));
 
   cosine = sqrt(1 - pow(sin(angle_rad), 2));
-  printf("The cosine of %.1f e %.4f. \n \n", angle_deg, cosine);
+  printf("The cosine of %.1f is %.4f. \n \n", angle_deg, cosine);
 
   printf("Ceiling of PI is %.1f. \n", ceil(PI_RAD));
   printf("Floor of PI is %.1f. \n", floor(PI_RAD));

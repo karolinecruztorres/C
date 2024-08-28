7f694872995f33a4f0d272939001711df6ffbd66
Karoline Cruz <karollcruz@hotmail.com>
2024-08-28 11:49:15 +0100

    feat: #4 loop exercise, a timer.

--- studies/13_loop_exercises.c
+++ studies/13_loop_exercises.c
@@ -50,7 +50,7 @@ int main(void)
 */
 
 // Exercise #3
-
+/*
 #include <stdio.h>
 
 int main(void)
@@ -75,6 +75,61 @@ int main(void)
     den = den + 2;
   }
   printf("Sum of %d first elements are %.3f", n, sum);
+
+  return 0;
+}
+*/
+
+// Exercise #4
+
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(_WIN64) // Windows
+#include <windows.h>
+#endif
+
+#ifdef __linux__ // Linux
+#include <unistd.h>
+#endif
+
+int main(void)
+{
+  size_t min, sec;
+  printf("Enter the time in minutes and seconds.");
+  scanf("%u %u", &min, &sec);
+
+  if (min > 59 || sec > 59 || (min == 0 && sec == 0))
+  {
+    printf("Enter a valid time, try again.");
+  }
+  else
+  {
+    do
+    {
+      system("cls");                   // clear
+      printf("%02u:%02u\n", min, sec); // '02' after the '%' to put a zero
+      Sleep(1000);                     // 1 second timer
+
+      if (sec == 0)
+      {
+        min--;
+        sec = 59;
+      }
+      else
+      {
+        sec--;
+      }
+    } while (min != 0 || sec != 0);
+    system("cls");
+    printf("%02u:%02u\n", min, sec);
+    printf("Time's up!");
+
+    for (int i = 0; i < 3; i++)
+    {
+      Beep(1500, 400); // a beep sound, first parameter is the decibels, the second is the time
+      Sleep(500);
+    }
+  }
 
   return 0;
 }
